607b771f08d1f27ea58474515b8125e34fffed7f
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-08-29 12:25:29 -0300

    ej3

---
+++ clase-2-29-agosto/ej3.py
@@ -0,0 +1,6 @@
+#Alumno: Terrile Mateo
+#División 111
+#3 Ingresar un número. Mostrar los números desde 0 hasta el número ingresado.
+numero = int(input("Ingrese un numero: "))
+for i in range(numero + 1):
+    print(i)
