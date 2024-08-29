505fba7aa2fe7b011bc840472da766faa9367732
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-08-29 10:21:24 -0300

    + test

--- clase-2-29-agosto/for.py
+++ clase-2-29-agosto/for.py
@@ -1,15 +1,44 @@
-rango = range(5,10)
-print(list(rango))
+# rango = range(5,10)
+# print(list(rango))
 
-rango = range(0,10,3)
-print(list(rango))
+# rango = range(0,10,3)
+# print(list(rango))
 
-rango = range(10,0,-1)
-print(list(rango))
+# rango = range(10,0,-1)
+# print(list(rango))
 
-for i in range(5):
-    print(i)
+# for i in range(5):
+#     print(i)
 
-lista = ["Azul", "Amarillo", "Rojo"]
-for color in lista:
-    print(color)
+# lista = ["Azul", "Amarillo", "Rojo"]
+# for color in lista:
+#     print(color)
+
+# acumulador = 0
+# for i in range(5):
+#     numero = input("Ingrese un numero: ")
+#     numero = int(numero)
+#     print(type(numero))
+#     acumulador +=numero
+
+# print(acumulador)
+
+# #Ingresar 20 numeros hasta encontrarme con un multiplo de 5
+
+# for i in range(5):
+#     numero = input("Ingrese un numero: ")
+#     numero = int(numero)
+#     if numero % 5 == 0:
+#         break
+#     print (numero)
+
+#Ingresar hasta 10 numeros solo mostrar los pares
+# for i in range(10):
+#     numero = input("ingrese un numero: ")
+#     numero = int(numero)
+
+#     if numero % 2 != 0:
+#         continue
+
+#     print(numero)
+
