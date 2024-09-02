bced3dcdf003215078cc5a2015f1fa06e83ec66e
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-02 09:36:38 -0300

    ej10

--- clase-2-29-agosto/ej10.py
+++ clase-2-29-agosto/ej10.py
@@ -1,2 +1,19 @@
 #Alumno: Terrile Mateo
 #División 111
+#10 Ingresar un número. Determinar si el número es primo o no.
+
+numero = int(input("Ingrese un número: "))
+es_primo = True 
+
+if numero < 2:
+    es_primo = False
+else:
+    for i in range(2, numero):
+        if numero % i == 0:
+            es_primo = False 
+            break
+
+if es_primo == True:
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
