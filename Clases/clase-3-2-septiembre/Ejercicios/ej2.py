1dd63b1876167cf1b654f943bc94af6ebdf39d00
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-05 12:30:02 -0300

    ej 2

--- Clases/clase-3-2-septiembre/Ejercicios/ej2.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej2.py
@@ -2,3 +2,15 @@
 #División 111
 #2
 # Escribe una función que calcule el área de un círculo. La función debe recibir el radio como parámetro y devolver el área.
+
+def determinar_area(radio):
+    area = 3.1416 * (radio ** 2)
+    return area
+
+#MAIN
+
+radio = float(input("Ingrese la radio: "))
+
+area = determinar_area(radio)
+
+print(f"El area del circulo: {area}")
