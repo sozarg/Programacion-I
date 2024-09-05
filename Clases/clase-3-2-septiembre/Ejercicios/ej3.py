34a24907753bc7d8784bee9f22fc168d09f41bfd
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-05 12:30:07 -0300

    ej 3

--- Clases/clase-3-2-septiembre/Ejercicios/ej3.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej3.py
@@ -2,3 +2,15 @@
 #División 111
 #3
 # Crea una función que verifique si un número dado es par o impar. La función debe imprimir un mensaje indicando si el número es par o impar.
+def verificar_paridad(paridad,mensaje_par,mensaje_impar):
+    paridad = input(paridad)
+    paridad = int(paridad)
+    if paridad % 2 == 0:
+        paridad = print(mensaje_par)
+    else:
+        paridad = print(mensaje_impar)
+        
+
+#MAIN
+
+paridad= verificar_paridad("Ingrese un numero: ", "Es par", "Es impar")
