8f02cb938cced857d39a50580b32a63d8811469e
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-05 12:30:10 -0300

    ej 4

--- Clases/clase-3-2-septiembre/Ejercicios/ej4.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej4.py
@@ -2,3 +2,13 @@
 #División 111
 #4
 # Crea una función que verifique si un número dado es par o impar. La función retorna True si el número es par, False en caso contrario.
+def determinar_paridad(numero:int)->bool:
+    if numero % 2 == 0:
+        es_par = True
+    else:
+        es_par = False
+        
+    return es_par
+##MAIN##
+resultado = determinar_paridad(7)
+print(resultado)
