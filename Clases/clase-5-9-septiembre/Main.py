c05f0092683b290128a39872c22fc9fadd89a0e1
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-09 09:44:06 -0300

    main

---
+++ Clases/clase-5-9-septiembre/Main.py
@@ -0,0 +1,8 @@
+# from Paquete.Funciones import *
+import Paquete.Funciones as PC
+# from Paquete.Funciones import *
+# saludar("Hola 111")
+# saludar_profesor("Pepe", "Biologia")
+# print(TAMAÑO_PANTALLA)
+
+PC.saludar("Hola a todos")
