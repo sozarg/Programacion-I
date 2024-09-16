c72397c09a6d806cf7df003f8cd813ff3438ab8c
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-16 09:19:33 -0300

    listas2

---
+++ Clases/clase-7-16-septiembre/listas2.py
@@ -0,0 +1,15 @@
+lista = [5, 9, 4, 3]
+
+print(lista)
+print(id(lista))
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+
+lista[1] = 999
+
+print(lista[1])
+
+print(id(lista))
