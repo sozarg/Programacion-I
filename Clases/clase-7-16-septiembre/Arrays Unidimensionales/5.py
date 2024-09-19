5349573d6ef7ddf92bec56be29d0cf13029e26b6
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-19 11:49:32 -0300

    ej 5 clase 7

---
+++ Clases/clase-7-16-septiembre/Arrays Unidimensionales/5.py
@@ -0,0 +1,4 @@
+#Alumno: Terrile Mateo
+#División 111
+#5
+#Escribir una función que reciba como parámetros una lista de enteros y muestre la/las posiciones en donde se encuentra el valor máximo hallado.
