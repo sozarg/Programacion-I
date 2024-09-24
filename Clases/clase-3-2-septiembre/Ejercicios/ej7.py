dbf0862330b1ca92de0475093e210d033ca05533
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-24 19:49:47 -0300

    7

--- Clases/clase-3-2-septiembre/Ejercicios/ej7.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej7.py
@@ -2,3 +2,19 @@
 #División 111
 #7
 # Crear una función que reciba un número y retorne True si el número es primo, False en caso contrario.
+def detectar_primos(numero:int) -> bool:
+    es_primo = True
+    if numero < 2:
+        es_primo = False
+    else:
+        for i in range(2, numero):
+            if numero % i == 0:
+                es_primo = False 
+            break
+    return es_primo
+#MAIN
+
+numero = int(input("Ingrese un numero: "))
+resultado = detectar_primos(numero)
+print(resultado)
+
