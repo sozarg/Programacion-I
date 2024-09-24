25b8c1b2264184d73da169dbfa86b1bced127bff
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-24 19:25:35 -0300

    ej6

--- Clases/clase-3-2-septiembre/Ejercicios/ej6.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej6.py
@@ -1,4 +1,16 @@
 #Alumno: Terrile Mateo
 #División 111
 #6
-# Diseña una función que calcule la potencia de un número. La función debe recibir la base y el exponente como argumentos y devolver el resultado.
+# Diseña una función que calcule la potencia de un número. La función debe recibir la base y el exponente como argumentos
+# y devolver el resultado.
+
+def calcular_potencia(base, exponente):
+    potencia = base ** exponente
+    return potencia
+
+#MAIN
+
+base = int(input("Ingrese la base: "))
+exponente = int(input("Ingrese el exponente: "))
+resultado = calcular_potencia(base, exponente)
+print(resultado)
