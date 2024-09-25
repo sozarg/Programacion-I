fc2b1c46c9cf64b0e3c122b18cb5f20b8df0882a
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-25 16:58:15 -0300

    ej 10

--- Clases/clase-3-2-septiembre/Ejercicios/ej10.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej10.py
@@ -4,10 +4,8 @@
 # Crear una función que le solicite al usuario el ingreso de un número entero y lo retorne.
 
 def pedir_numero():
-    numero = input("Ingrese un numero: ")
-    numero = int(numero)
-    
+    numero = int(input("Ingrese un numero: "))    
     return numero
 
-numero = pedir_numero()
-print(f"El numero es: {numero}")
+resultado = pedir_numero()
+print(f"El numero es: {resultado}")
