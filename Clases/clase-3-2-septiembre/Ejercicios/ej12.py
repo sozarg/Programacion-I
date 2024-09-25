8d4c8f741d80add9d30a85215c4a6760c1cbef69
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-25 16:58:24 -0300

    ej 12

--- Clases/clase-3-2-septiembre/Ejercicios/ej12.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej12.py
@@ -2,3 +2,9 @@
 #División 111
 #12
 # Crear una función que le solicite al usuario el ingreso de una cadena y la retorne. 
+def pedir_cadena():
+    cadena = input("Ingrese una cadena: ")    
+    return cadena
+
+resultado = pedir_cadena()
+print(f"La cadena es: {resultado}")
