1c249d0795bc124596399e41265bce4d512aaf61
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-25 16:58:27 -0300

    ej 13

--- Clases/clase-3-2-septiembre/Ejercicios/ej13.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej13.py
@@ -2,3 +2,13 @@
 #División 111
 #13
 # Especializar las funciones del punto 1, 2 y 3 para hacerlas reutilizables. Agregar validaciones.
+
+def pedir_valores():
+    
+    
+
+valor_entero = int(input("Ingrese un numero entero: "))
+valor_flotante = int(input("Ingrese un valor flotante: "))
+valor_cadena = int(input("Ingrese"))
+resultado = pedir_valores()
+print(f"")
