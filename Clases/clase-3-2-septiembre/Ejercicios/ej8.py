1f63b4ddfe73b34389863f34fcf153fe2ca04d1d
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-25 16:58:06 -0300

    ej 8

--- Clases/clase-3-2-septiembre/Ejercicios/ej8.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej8.py
@@ -1,4 +1,24 @@
 #Alumno: Terrile Mateo
 #División 111
 #8
-# Crear una función que (utilizando la función del punto 11), muestre todos los números primos comprendidos entre entre la unidad y un número ingresado como parámetro. La función retorna la cantidad de números primos encontrados.
+#Crear una función que (utilizando la función del punto 11 de la guía de For), muestre todos los números primos comprendidos entre entre la unidad 
+# y un número ingresado como parámetro. La función retorna la cantidad de números primos encontrados.
+
+def primos_encontrados(numero):
+    contador_primos = 0
+    for x in range(2, numero + 1):
+        es_primo = True
+
+        for i in range(2, x):
+            if x % i == 0:
+                es_primo = False
+                break
+
+        if es_primo == True:
+            print(f"{x} Es primo")
+            contador_primos += 1
+    return contador_primos
+
+numero = int(input("Ingrese un numero: "))
+resultado = primos_encontrados(numero)
+print(f"Se encontraron {resultado} numeros primos")
