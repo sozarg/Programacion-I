3798f27ecc87666a163bc31025daaf1d0e3a418d
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-09-25 16:58:19 -0300

    ej 11

--- Clases/clase-3-2-septiembre/Ejercicios/ej11.py
+++ Clases/clase-3-2-septiembre/Ejercicios/ej11.py
@@ -2,3 +2,9 @@
 #División 111
 #11
 # Crear una función que le solicite al usuario el ingreso de un número flotante y lo retorne.
+def pedir_numero():
+    numero = float(input("Ingrese un numero: "))    
+    return numero
+
+resultado = pedir_numero()
+print(f"El numero es: {resultado}")
