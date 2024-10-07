d94e6465b36b360a834e5234f8c0a9583c11934c
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-10-07 09:37:34 -0300

    cadenas

---
+++ Clases/clase-30-7-octubre/cadenas.py
@@ -0,0 +1,20 @@
+# cadena = "hola comision 111"
+
+# sub_cadena = cadena[:]
+# print(sub_cadena)
+
+# sub_cadena = cadena[3:9] #range
+# print(sub_cadena)
+
+# sub_cadena = cadena[5:]
+# print(sub_cadena)
+
+# sub_cadena = cadena[:9]
+# print(sub_cadena)
+
+def my_slicing(cadena: str, inicio: int, fin: int) -> str:
+    for i in range(inicio, fin):
+        print(cadena[i], end = "")
+cadena = "hola comision 111"
+
+my_slicing(cadena, 5,12)
