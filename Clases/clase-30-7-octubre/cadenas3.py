c08f73461372d742b5f5dcdcde8477ab734d10a0
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-10-07 10:25:09 -0300

    cadenas3

---
+++ Clases/clase-30-7-octubre/cadenas3.py
@@ -0,0 +1,11 @@
+cadena = "hola"
+
+print(cadena == "hola")
+print(cadena != "hola")
+print(cadena == "Hola")
+print(cadena > "hola")
+print(cadena > "amarillo")
+
+cadena = "hola"
+mayusculas = cadena.upper()
+print(mayusculas)
