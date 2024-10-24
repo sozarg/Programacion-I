75c2d5147cde596260a06aec174eb848864395c3
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-10-24 10:37:01 -0300

    add

---
+++ Clases/clase-16-24-octubre/demo.py
@@ -0,0 +1,9 @@
+# mi_cadena = "     german     "
+# mi_cadena = mi_cadena.strip()
+# print(f"--{mi_cadena}--")
+
+###########
+
+mi_cadena = "hoLa"
+print(mi_cadena.upper())
+print(mi_cadena.lower())
---
+++ Clases/clase-6-12-septiembre/ejercicios/ej4.py
@@ -0,0 +1,7 @@
+#Alumno: Terrile Mateo
+#División 111
+#4
+#Realizar una función para calcular el número de Fibonacci de un número ingresado por consola. La función deberá seguir el siguiente prototipo
+
+def calcular_fibonacci(numero:int )->int:
+    pass
