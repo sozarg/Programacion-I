85138740bf7c4f21e4970781264330e51d2a0bc0
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-11-04 08:32:30 -0300

    d

---
+++ Clases/clase-16-24-octubre/parcialhecho.py
@@ -0,0 +1,14 @@
+
+# #mostrar
+# def cal
+# mostar_total_inventario(total_inventario,sucursales)
+
+# #########################################
+
+# inventario = [
+#     [128, 355, 422],
+# ]
+
+# nombres_productos = ["Notebook", "Smartphone", "Tablet"]
+# precios = [999, 1499, 799]
+# LEN_INVENTARIO = len(inventario)
